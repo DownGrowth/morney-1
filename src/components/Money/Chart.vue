cfd66902c7d030d07b630b226121365f426449ad
BK <1209157041@qq.com>
2022-09-07 21:15:19 +0800

    自动更新options

--- src/components/Money/Chart.vue
+++ src/components/Money/Chart.vue
@@ -3,18 +3,24 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'vue-property-decorator';
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
 import * as echarts from 'echarts';
 @Component
 export default class Chart extends Vue{
     @Prop() options?: echarts.EChartsOption
+    chart?:echarts.ECharts
     mounted() {
         if (this.options === undefined) {
             return console.error('options为空')
         }
-        const chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
-        chart.setOption(this.options)
-}
+        this.chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
+        this.chart.setOption(this.options)
+    }
+
+    @Watch('options') 
+    onOptionsChange(newValue: echarts.EChartsOption){
+        this.chart!.setOption(newValue)
+    }
 }
 </script>
 
